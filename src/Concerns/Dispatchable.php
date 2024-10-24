e4c23472b8150986ae94cc60344c92f0a04a56ec
Bradie Tilley <bradie@hatchet.com.au>
2024-10-24 10:26:18 +0800

    phpstan fixes

--- src/Concerns/Dispatchable.php
+++ src/Concerns/Dispatchable.php
@@ -3,16 +3,20 @@
 namespace BradieTilley\Actions\Concerns;
 
 use BradieTilley\Actions\Contracts\Dispatcher;
+use Illuminate\Foundation\Application;
 
 trait Dispatchable
 {
     public static function dispatch(mixed ...$arguments): mixed
     {
+        /** @var Application $app */
+        $app = app();
+
         /** @var Dispatcher $dispatcher */
         $dispatcher = app(Dispatcher::class);
 
         /** @var class-string<static> */
-        $class = app()->getAlias(static::class);
+        $class = $app->getAlias(static::class);
 
         /** @phpstan-ignore-next-line */
         $action = new $class(...$arguments);
